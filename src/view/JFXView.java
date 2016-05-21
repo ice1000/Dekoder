da933566987e25008921865ca3cbf5b4a60d9101
千里冰封 <zhangyinsen@126.com>
2016-05-21 18:27:59 +0800

    现在开始做GUI部分
    Former-commit-id: c8639f721ebb6938f22d55dcc8632ee2abf5e2ee

---
+++ src/view/JFXView.java
@@ -0,0 +1,18 @@
+package view;/**
+ * Created by asus1 on 2016/5/21.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JFXView extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
