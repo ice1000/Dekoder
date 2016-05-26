3c9ceead662022f426f358b8789dd4399adac450
ice1000 <951394653@qq.com>
2016-05-26 23:37:39 +0800

    反正百度上是这么说的
    Former-commit-id: 18fce453f7e72e8a9a29b3948991ca4d0cbe64fd
    Former-commit-id: b626856ba95807c5019d0ad46eaa8e2a2aa65e39

---
+++ src/decoder/MIDIDecoder.kt
@@ -0,0 +1,40 @@
+package decoder
+
+import utils.Echoer
+import java.io.File
+import javax.sound.midi.MidiSystem
+import javax.sound.midi.Sequence
+import javax.sound.midi.Sequencer
+
+/**
+ * @author ice1000
+ * TODO: implement midi decoding
+ * Created by ice1000 on 2016/5/24.
+ */
+
+class MIDIDecoder : DecoderInterface {
+    override var path: String
+    private var seq: Sequence? = null
+    private var seqr: Sequencer? = null
+
+    override fun play(): Unit? = seqr?.start()
+
+    override fun stop(): Unit? = Unit
+
+    override fun init() {
+        seq = MidiSystem.getSequence(File(path))
+        seqr = MidiSystem.getSequencer()
+        seqr?.open()
+        seqr?.sequence = seq
+        seqr?.addMetaEventListener {
+            if (it.type == 47)
+                println("done playing")
+        }
+    }
+
+    override fun getTotalTime(): Long = 60L
+
+    constructor(path: String, echoer: Echoer) : super(echoer) {
+        this.path = path
+    }
+}
